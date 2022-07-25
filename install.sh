1e10e4d748c58920164243c58c81b92edb30bbde
Sean R Milligan <sean@sean.codes>
2022-07-25 00:43:17 -0700

    Reorganize sublime and terminator packages so Stow handles ~/.config subdirectories

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+while ! command -v stow 2>&1 > /dev/null
+do
+  read -r -p "GNU Stow not installed. Install? [y/N] " option
+  if [ "$option" == "y" ] || [ "$option" == "Y" ]
+  then
+      sudo apt install stow
+  else
+    exit 1
+  fi
+done
+
 mkdir --parents $HOME/.config/terminator
 mkdir --parents $HOME/.config/sublime-text-3/Packages/User
-stow --dir=$HOME/dotfiles --target=$HOME bash gdb nano tmux vim
-stow --dir=$HOME/dotfiles --target=$HOME/.config/terminator terminator
-stow --dir=$HOME/dotfiles --target=$HOME/.config/sublime-text-3/Packages/User sublime
+stow --dir=$HOME/dotfiles --target=$HOME bash gdb nano sublime terminator tmux vim
