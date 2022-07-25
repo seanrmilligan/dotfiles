db399d95dbbbdb2c1d8bd74b779c2b61c441ea03
Sean R Milligan <sean@sean.codes>
2022-07-25 01:11:05 -0700

    Forcefully install over local versions of dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! command -v stow 2>&1 > /dev/null
+while ! command -v stow > /dev/null 2>&1
 do
   read -r -p "GNU Stow not installed. Install? [y/N] " option
   if [ "$option" == "y" ] || [ "$option" == "Y" ]
@@ -14,3 +14,4 @@ done
 mkdir --parents $HOME/.config/terminator
 mkdir --parents $HOME/.config/sublime-text-3/Packages/User
 stow --dir=$HOME/dotfiles --target=$HOME bash gdb nano sublime terminator tmux vim
+git -C $HOME/dotfiles checkout . > /dev/null 2>&1
