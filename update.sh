e389a91c3329dcc3085940914adf57c8fbea5ecf
Sean Milligan <hello@sean.codes>
2023-07-05 21:26:53 -0700

    Include SSH files in stow management

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 mkdir --parents $HOME/.config/terminator
 mkdir --parents $HOME/.config/sublime-text/Packages/User
-stow --restow --dir=$HOME/dotfiles --target=$HOME bash gdb nano tmux vim
+stow --restow --dir=$HOME/dotfiles --target=$HOME bash gdb git nano profile ssh tmux vim
 stow --restow --dir=$HOME/dotfiles --target=$HOME/.config/terminator terminator
 stow --restow --dir=$HOME/dotfiles --target=$HOME/.config/sublime-text/Packages/User sublime
